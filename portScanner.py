fb7083b3531ae3e0cefd32f8695b3926095a346a
Nick <namct147@gmail.com>
2026-03-12 16:11:40 +0700

    my port scanner with multithreading upgrade

--- portScanner.py
+++ portScanner.py
@@ -1,6 +1,10 @@
 import socket
 import pyfiglet
 
+#new:
+import concurrent.futures
+#can i use from multiprocessing import Pool? (saw it on stack overflow)
+
 ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
 print(ascii_banner)
 
@@ -9,24 +13,32 @@ target_ip = socket.gethostbyname(target)
 
 #co 65535 cong ma nhieu qua nen chi lay 1025 cong thoi hoac chay tu 70 len 90
 
-for i in range (70, 90):
-    port = i
+#use func to guide workers
+def scan_port(port):
+    global target_ip # use target ip globally
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     s.settimeout(0.25)
-
+    
     result = s.connect_ex((target_ip, port))
-
-    if(result == 0):
+     
+    if result == 0: #connect success
         print(f"Connect success!! Port {port} is open")
         try:
-            s.send(b"WhoAreYou\r\n")
-            
-            message = s.recv(1024)
-            #they send boring stuff so no need            
-            #print(f"{message} said: {message.decode().strip()}") 
+            s.send(b"WhoAreYou\r\n") # \r moves the cursor back to the start
+            message = s.recv(1024) #1024 characters
         except:
-            print("This guy is tuff, no respone!")
-        
-    #else: print(f"ERROR! Portal {port} closed")
+            pass
+    
     s.close()
+    
+ports_to_scan = range(1,1025) #can i really scan all 65535 ports
+### in theory i can do it, but the big leauges like google, facebook and their ddos system will identify me as their opp
+
+print(f"Scanning {target_ip}...\n")
+
+#executor will hire 100 workers to work and use map to guide them
+with concurrent.futures.ThreadPoolExecutor(max_workers = 100) as executor:
+    executor.map(scan_port, ports_to_scan)
+    
+print("Scan completed!!")
